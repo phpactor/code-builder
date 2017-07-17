c1b8fd8f1ab6e78173aabab2973d5dd2dc7ce401
dantleech <dan.t.leech@gmail.com>
2017-07-17 11:47:02 +0100

    Properties

--- lib/Domain/Prototype/Properties.php
+++ lib/Domain/Prototype/Properties.php
@@ -6,7 +6,10 @@ class Properties extends Collection
 {
     public static function fromProperties(array $properties)
     {
-        return new self($properties);
+        return new static(array_reduce($properties, function ($acc, $property) {
+            $acc[$property->name()] = $property;
+            return $acc;
+        }, []));
     }
 
     protected function singularName(): string
