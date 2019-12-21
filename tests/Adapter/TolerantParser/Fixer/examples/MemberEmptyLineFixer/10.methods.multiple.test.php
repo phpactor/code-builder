1a1857cb69794bb6a777e44acc329450af4eb025
Daniel Leech <dan.t.leech@gmail.com>
2019-12-21 15:29:25 +0000

    Ensure always empty line between mehtods

---
+++ tests/Adapter/TolerantParser/Fixer/examples/MemberEmptyLineFixer/10.methods.multiple.test.php
@@ -0,0 +1,25 @@
+// File: source.php
+<?php
+
+class TestClass
+{
+    public function foo()
+    {
+    }
+    public function bar()
+    {
+    }
+}
+// File: expected.php
+<?php
+
+class TestClass
+{
+    public function foo()
+    {
+    }
+
+    public function bar()
+    {
+    }
+}
