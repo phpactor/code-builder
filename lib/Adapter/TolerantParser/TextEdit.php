268bd757dc90db77872250984cabbe22ba038ef9
Daniel Leech <dan.t.leech@gmail.com>
2019-12-21 19:20:08 +0000

    Add deprecation for old location of TextEdit

---
+++ lib/Adapter/TolerantParser/TextEdit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Phpactor\CodeBuilder\Adapter\TolerantParser;
+
+use Phpactor\CodeBuilder\Domain\TextEdit as DomainTextEdit;
+
+/**
+ * @deprecated use Phpactor\CodeBuilder\Domain\TextEdit instead.
+ */
+class TextEdit extends DomainTextEdit
+{
+}
