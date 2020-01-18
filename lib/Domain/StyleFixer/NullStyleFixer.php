6cbf6a3c897a6d2874541e10cfa0b7a2acd8bb10
Daniel Leech <dan.t.leech@gmail.com>
2020-01-18 13:31:49 +0000

    Fix null style fixer

--- lib/Domain/StyleFixer/NullStyleFixer.php
+++ lib/Domain/StyleFixer/NullStyleFixer.php
@@ -3,10 +3,16 @@
 namespace Phpactor\CodeBuilder\Domain\StyleFixer;
 
 use Phpactor\CodeBuilder\Domain\StyleFixer;
+use Phpactor\CodeBuilder\Domain\TextEdits;
 
 class NullStyleFixer implements StyleFixer
 {
     public function fix(string $code): string
+    {
+        return $code;
+    }
+
+    public function fixIntersection(TextEdits $edits, string $code): string
     {
         return $code;
     }
